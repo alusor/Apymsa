c1cea08b544369a54ca13d3c4b112a2e7cf94aca
Alusor <eddvelez@outlook.com>
2015-08-03 09:49:58 -0500

    Register on tecnofuel site

--- Assets/Scripts/Init/DialogControl.cs
+++ Assets/Scripts/Init/DialogControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DialogControl : MonoBehaviour {
+	public GameObject sis;
+	public GameObject reg;
 	string[] message = new string[]{
 		"Bienvenido a Tecnofuel Racing a continuación te explicare algunas cosas importantes para que puedas jugar.",
 		"Si quieres ponerte a correr dirígete a carrera, o si quieres puedes ir al garage, antes de empezar una carrera recuerda que tienes que pisar el acelerador!",
@@ -13,17 +15,42 @@ public class DialogControl : MonoBehaviour {
 	public Text box;
 	// Use this for initialization
 	void Start () {
+		/*foreach (Transform child in reg.transform) {
+			child.gameObject.SetActive(false);
+		}*/
+		reg.gameObject.SetActive (false);
 		StartCoroutine (instructions());
 	}
 	IEnumerator instructions(){
-		for(int i=0;i<message.Length;i++){
-			for(int j=0;j<=message[i].Length;j++){
-				box.text = message[i].Substring(0,j);
-				yield return new WaitForSeconds (.05f);
+		if (!PlayerPrefs.HasKey ("instructions")) {
+			for (int i=0; i<message.Length; i++) {
+				for (int j=0; j<=message[i].Length; j++) {
+					box.text = message [i].Substring (0, j);
+					yield return new WaitForSeconds (.02f);
+				}
+				yield return new WaitForSeconds (2);
 			}
-			yield return new WaitForSeconds (2);
-		}
+			PlayerPrefs.SetInt ("instructions", 0);
+			reg.transform.gameObject.SetActive(true);
 
+			/*GameObject temp =  Instantiate(reg, new Vector3(0,0,0),Quaternion.identity) as GameObject;
+			temp.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>());
+			temp.GetComponent<RectTransform>().offsetMin = sis.GetComponent<RectTransform>().offsetMin;
+			temp.GetComponent<RectTransform>().offsetMax = sis.GetComponent<RectTransform>().offsetMax;
+			SaveData t1 =  gameObject.GetComponent<SaveData>();
+			t1.name = GameObject.Find("Apodo").GetComponent<Text>();
+			Button agree = GameObject.FindGameObjectWithTag("button").GetComponent<Button>();
+			Debug.Log(agree.name); 
+			agree.onClick.AddListener(() => {t1.get();});
+			t1.reg = temp;*/
+
+			//temp.GetComponentInChildren<Button>().onClick.AddListener(()=>{FindObjectOfType<SaveData>().get();});
+			//temp.GetComponent<RectTransform>().offsetMin  = new Vector2(0,0);
+			//temp.GetComponent<RectTransform>().offsetMin =  new Vector2(0,0);
+		} else
+			Destroy (reg);
+
+		Destroy (sis);
 	}
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/Init/SaveData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SaveData : MonoBehaviour {
+	public Text name;
+	public Text mail;
+	public Text mess;
+	public string url;
+	public GameObject reg;
+	// Use this for initialization
+	public void get(){
+		if (name.text.Length > 0 && mail.text.Length > 0) {
+			PlayerPrefs.SetString ("Name", name.text);
+			PlayerPrefs.SetString ("email", mail.text);
+			StartCoroutine(uploadNewUser());
+		} else {
+			mess.text = "Ingresa ambos datos.";
+		}
+
+	}
+	IEnumerator uploadNewUser(){
+		WWW connect = new WWW (url+"insertData.php?email="+mail.text+"&score="+0.ToString());
+		yield return connect;
+		Destroy (reg);
+	}
+}
